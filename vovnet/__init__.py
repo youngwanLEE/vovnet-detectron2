d1fc66436fbb04c3c474a53eed2dee980ddf7a81
youngwanLEE <youngwan88@gmail.com>
2020-02-26 07:08:40 +0000

    add MobileNetV2 backbone

--- vovnet/__init__.py
+++ vovnet/__init__.py
@@ -1,3 +1,4 @@
 # Copyright (c) Youngwan Lee (ETRI) All Rights Reserved.
 from .config import add_vovnet_config
 from .vovnet import build_vovnet_fpn_backbone, build_vovnet_backbone
+from .mobilenet import build_mobilenetv2_fpn_backbone, build_mnv2_backbone
